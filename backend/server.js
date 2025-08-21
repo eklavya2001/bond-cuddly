77986433ffca1baa8c6583f1f0963a29067b78cd
Eklavya <eklavyamishra6@gmail.com>
2025-08-21 15:34:38 +0530

    updated frontend url

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,11 @@ dotenv.config();
 const app = express();
 connectDB();
 
-app.use(cors());
+app.use(cors({
+  origin: "https://bond-cuddly.onrender.com", // frontend URL
+  methods: ["GET", "POST", "PUT", "DELETE"], // allowed HTTP methods
+  credentials: true, // allow cookies if needed
+}));
 app.use(express.json());
 
 app.use("/api/bond", require("./routes/bondRoutes"));
